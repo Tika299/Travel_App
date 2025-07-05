c256a377b7e71aca86aa643c5224ca4e591b0c14
vukha12 <kha15964@gmail.com>
2025-07-05 12:25:19 +0700

    done-database

--- backend/app/Models/Hotel.php
+++ backend/app/Models/Hotel.php
@@ -9,28 +9,27 @@ class Hotel extends Model
     protected $fillable = [
         'name',
         'description',
-        'address',
-        'latitude',
-        'longitude',
         'rating',
-        'contact_info',
-        'wheelchair_access',
+        'phone_number',
+        'main_image_url',
     ];
 
     protected $casts = [
-        'latitude' => 'float',
-        'longitude' => 'float',
         'rating' => 'float',
-        'wheelchair_access' => 'boolean',
     ];
 
-   public function reviews()
-{
-    return $this->morphMany(Review::class, 'reviewable');
-}
-public function visitedByUsers()
-{
-    return $this->morphMany(UserVisitedPlace::class, 'place');
-}
+    public function reviews()
+    {
+        return $this->morphMany(Review::class, 'reviewable');
+    }
 
+    public function visitedByUsers()
+    {
+        return $this->morphMany(UserVisitedPlace::class, 'place');
+    }
+
+    public function hotelrooms()
+    {
+        return $this->hasMany(HotelRoom::class);
+    }
 }
