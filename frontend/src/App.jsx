cf3a2c6c2689cdc62e7f5a94d552304c952d50f4
nvd1402 <nguyenvandai89@gmail.com>
2025-07-13 10:17:39 +0700

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,22 @@
-import Header from './components/ui/schedule/Header';
+import { useState } from "react";
 import Sidebar from './components/ui/schedule/Sidebar';
 import CalendarFull from './components/ui/schedule/CalendarFull';
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
-      <Header />
       <div className="flex flex-1">
-        <Sidebar />
-        <div className="flex-1 p-2">
+        {/* Sidebar */}
+        {isSidebarOpen && <Sidebar />}
+        <div className={`transition-all duration-300 ${isSidebarOpen ? "flex-1" : "w-full"} p-2`}>
+          {/* Đã xóa ScheduleHeader ở đây */}
           <CalendarFull />
         </div>
       </div>
     </div>
   );
 }
-
 export default App;
+
