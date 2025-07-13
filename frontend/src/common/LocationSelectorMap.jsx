e9603edf338e04b49d413ea22709eff442262e30
LeHongSon-2005 <23211tt4339@mail.tdc.edu.vn>
2025-07-13 10:51:57 +0700

    sua map

--- frontend/src/common/LocationSelectorMap.jsx
+++ frontend/src/common/LocationSelectorMap.jsx
@@ -69,7 +69,8 @@ const LocationSelectorMap = ({ onLocationSelect, initialLatitude, initialLongitu
       <ChangeMapView center={selectedPosition} zoom={13} />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="
+https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
       />
       {selectedPosition && (
         <Marker
