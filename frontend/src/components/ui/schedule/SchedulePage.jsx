806129d7753ba1cfa79e1dcc1f71b1fe01f6b0d4
nvd1402 <nguyenvandai89@gmail.com>
2025-07-18 01:34:36 +0700

    .

--- frontend/src/components/ui/schedule/SchedulePage.jsx
+++ frontend/src/components/ui/schedule/SchedulePage.jsx
@@ -14,9 +14,9 @@ export default function SchedulePage() {
   };
 
   return (
-    <div className="flex w-full h-screen bg-gray-50">
+    <div className="flex w-full min-h-screen bg-gray-50 overflow-hidden">
       <Sidebar onCreateEvent={handleCreateEvent} />
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col min-h-screen">
         <CalendarFull ref={calendarRef} />
       </div>
     </div>
