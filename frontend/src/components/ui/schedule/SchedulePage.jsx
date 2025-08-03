c7ad6e629fa61d51cfcf51ee86faf5f828c1ae95
Tika299 <vukofa9120@gmail.com>
2025-08-03 13:36:02 +0700

    commit add header for SchedulePage

--- frontend/src/components/ui/schedule/SchedulePage.jsx
+++ frontend/src/components/ui/schedule/SchedulePage.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react';
 import Sidebar from './Sidebar';
 import CalendarFull from './CalendarFull';
+import Header from '../../Header';
 
 export default function SchedulePage() {
   const calendarRef = useRef();
@@ -13,10 +14,14 @@ export default function SchedulePage() {
   };
 
   return (
-    <div className="flex w-full min-h-screen bg-gray-50 overflow-hidden">
-      <Sidebar onCreateEvent={handleCreateEvent} />
-      <div className="flex-1 flex flex-col min-h-screen">
-        <CalendarFull ref={calendarRef} />
+    <div className="w-full min-h-screen bg-gray-50 overflow-hidden">
+      <Header />
+      {/* Main content area */}
+      <div className='flex w-full'>
+        <Sidebar onCreateEvent={handleCreateEvent} />
+        <div className="flex-1 flex flex-col min-h-screen">
+          <CalendarFull ref={calendarRef} />
+        </div>
       </div>
     </div>
   );
